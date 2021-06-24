2b267019d2ced8a051237d177a6d2caa9ee96afc
Daniel Soto <dasg.1993@gmail.com>
2021-06-23 21:07:23 -0500

    [feature] - implement post services list & use ref & useEffect mounted component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,71 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
-import { getList } from './shared/services/list';
+import { getList, setItem } from './shared/services/list';
 
 const  App = () => {
-  const [list, setList] = useState([])
+  const [list, setList] = useState([]);
+  const [itemInput, setItemInput] = useState('');
+  const [alert, setAlert] = useState(false);
+  const [error, setError] = useState(false);
+  let mounted = useRef(true);
 
   useEffect(() => {
-    let mounted = true;
+    mounted.current = true;
+    if(list.length && !alert) return;
     getList()
       .then((items) => {
-        if(mounted) {
-          setList(items)
+        if(mounted.current){
+          setList(items);
         }
       })
-    return () => mounted = false
-  }, [])
+    return () => mounted.current = false;
+  }, [alert,list])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setItem(itemInput)
+      .then(() => {
+        if(mounted.current){
+          setItemInput('')
+          setAlert(true)
+          setError(false)
+        }
+      })
+      .catch(() => {
+        setError(true)
+      })
+  }
+
+  useEffect(() => {
+    if(alert) {
+      setTimeout(() => {
+        if(mounted.current) {
+          setAlert(false);
+        }
+      }, 1000)
+    }
+  }, [alert]);
+
   return (
     <div className="wrapper">
       <h1>My Grocery List</h1>
       <ul>
-        {list.map((item) => <li>{item.product}</li>)}
+        {list.map((item, index) => <li key={index}>{item.product}</li>)}
       </ul>
+      {alert && <h2>Submit successful</h2>}
+      {error && <h2>Submit error</h2>}
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>New Item</p>
+          <input
+            type="text"
+            onChange={event => setItemInput(event.target.value)}
+            value={itemInput}
+            />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
--- src/shared/services/list.js
+++ src/shared/services/list.js
@@ -2,3 +2,14 @@ export const getList = () => {
     return fetch('http://localhost:3333/list')
     .then(data => data.json())
 }
+
+export const setItem = (item) => {
+    return fetch('http://localhost:3333/list', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ product: item })
+    })
+    .then(data => data.json())
+} 
