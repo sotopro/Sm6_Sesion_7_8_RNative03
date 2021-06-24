3482b99f2ee821f9f36ddb364621efba46bdae23
Daniel Soto <dasg.1993@gmail.com>
2021-06-23 19:17:23 -0500

    [feature] - clean create react app project & create class app component with state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {text: 'Learn about React'},
+      {text: 'Meet friends for lunch'},
+      {text: 'build really cool todo app'}
+    ]
+  }
+
+  setTodos = (todos) => this.setState({todos});
+
+  render() {
+    return (
+      <div className="app">
+        <h1>Todo App</h1>
+        {this.state.todos.map((item) => (
+          <p>{item.text}</p>
+        ))}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
